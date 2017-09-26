fdf301427d937b6193bd79dbddacf00a0deb0686
Jason Perkins <starkos@industriousone.com>
2017-09-26 10:54:12 -0400

    Add react-router, set up Home and 404 routes

---
+++ src/containers/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Wordmark from '../components/Wordmark';
+
+
+export default class NotFound extends React.Component {
+	render() {
+		return (
+			<div className="NotFound">
+				<h1>Page not found!</h1>
+			</div>
+		);
+	}
+}
